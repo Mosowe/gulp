0d78c25c748f1e3095d50e857e94cb5caaec2a41
mosowe <630834853@qq.com>
2020-10-16 10:51:59 +0800

    自定义封装原生ajax

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -9,16 +9,48 @@ function myAjax (params) {
     }else{
       request=new ActiveXObject("Microsoft.XMLHTTP");
     }
-
+    // get请求，拼接参数在url后面
+    if (params.method && params.method.toLowerCase() === 'get' || !params.method) {
+      let data = ''
+      for (const key in params.data) {
+        data ? data += `&${key}=${params.data[key]}` : data = `?${key}=${params.data[key]}`
+      }
+      params.url = params.url + data
+    }
+    
     request.open(
       params.method?params.method:'get',
       params.url,
       params.async ? params.async : true
     ); 
+    /*********** 配置请求头 ************/
+    /*********** setRequestHeader必须写在open和send中间  ************/
+
+    request.setRequestHeader("accept", "application/json, text/plain, */*");
+    // params附带其他请求头
+    let hasContentType = false
     for (const key in params.headers) {
-      request.setRequestHeader(key,params.headers[key]);  //必须写在open和send中间 
+      request.setRequestHeader(key,params.headers[key]);
+      if (key.toLowerCase() === 'content-type') {
+        hasContentType = true
+      }
     }
-    request.send();
+    // get请求
+    if (params.method && params.method.toLowerCase() === 'get' || !params.method) {
+      hasContentType ? '': request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    } else {
+      if (!params.file) { // 非文件上传的其他请求
+        hasContentType ? '': request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      }
+    }
+    /*********** end 配置请求头 ************/
+    if (params.file) { // 文件数据
+      request.send(params.file);
+    } else{ // 一般数据
+      request.send(JSON.stringify(params.data) || null);
+    }
+    
+
     request.onreadystatechange = () => {
       if(request.readyState === 4){
         if(request.status === 200){
